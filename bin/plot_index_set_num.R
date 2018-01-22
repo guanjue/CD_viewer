799aba84c152f7f9ae449b45c83f886c23601edf
Guanjue Xiang <gzx103@E1-052111.science.psu.edu>
2018-01-22 18:53:15 -0500

    deactivate index plotting

--- bin/plot_index_set_num.R
+++ bin/plot_index_set_num.R
@@ -12,6 +12,7 @@ binary_matrix = as.matrix(read.table(binary_matrix_file, header=FALSE))
 index_set_table = table(binary_matrix)
 index_set_num = index_set_table
 
+index_set_table_thresh = index_set_table[index_set_table>=limline]
 
 if (uselog2=='T'){
 	index_set_num = log2(index_set_num+smallnum)
@@ -25,3 +26,7 @@ if (uselog2=='T'){
 	abline(v=(limline), col='red')
 }
 dev.off()
+
+
+
+write.table(index_set_table_thresh, paste(output_file, '.binary.counts.thresh.bed', sep=''), sep='\t', quote = FALSE, row.names = TRUE, col.names = FALSE)
