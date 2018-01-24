6650bf0ef66f1d572accaea32420e2d6cde3a70d
Guanjue Xiang <gzx103@E1-052111.science.psu.edu>
2018-01-24 16:14:42 -0500

    violin

--- bin/plot_violin.py
+++ bin/plot_violin.py
@@ -40,6 +40,8 @@ def plot_violin(input_file_list, outputname, log2, small_num, lowerlim, upperlim
 
 	### plot violin plot
 	pos = range(1,len(filename_list)+1*5,5)
+	print(len(pos))
+	print(len(signal_track_list))
 	print('plot violinplot of index:' + outputname)
 	plt.figure(figsize=(10,10), dpi=200)
 	plt.violinplot(signal_track_list, pos, points=50, widths=4, showmeans=True, showextrema=False, showmedians=False)
