d73c6c51355065e916cd6813af02e81b3b6f7515
Universe <universe@MacBook-Air.local>
2018-02-09 13:59:07 -0500

    increase node size

--- bin/plot_tree_multi_color_filter.R
+++ bin/plot_tree_multi_color_filter.R
@@ -134,10 +134,10 @@ for (i in seq(1,dim(signal_matrix_color)[1])){
 	### sort colnames by tree nodes id
 	match_id = match(tree_names, colname)
 	V(tree.igraph)$color = value_col[match_id]
-	V(tree.igraph)$size = 50
+	V(tree.igraph)$size = 40
 
 	png(paste(signal_input_list, index_set_name[i], '.tree.sh.png', sep = ''), width = 800, height = 800)
-	plot(tree.igraph, layout = layout_as_tree(tree.igraph, root=c(1)))
+	plot(tree.igraph, layout = layout_as_tree(tree.igraph, root=c(1)), layout=l2)
 	dev.off()
 }
 
