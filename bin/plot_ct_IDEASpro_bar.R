3112891fb24df24e523eb9aa150de0267afd7be4
Universe <universe@MacBook-Air.local>
2018-02-12 02:17:35 -0500

    add plot bar

--- bin/plot_ct_IDEASpro_bar.R
+++ bin/plot_ct_IDEASpro_bar.R
@@ -6,15 +6,6 @@ ideas_state_color = args[3]
 cREs_IDEASpro_outfile = args[4]
 
 ### read index set matrix
-read_matrix = function(inputfile){
-	data_index_set = as.matrix(read.table(inputfile, header=T))
-	rownames(data_index_set) = data_index_set[,1]
-	data_index_set = data_index_set[,-1]
-	class(data_index_set) = "numeric" 
-	return(data_index_set)
-}
-
-### read index set matrix
 read_color = function(x){
 	rgb_color_int = as.numeric(unlist(strsplit(x, ',')))
 	rgb_color = rgb(rgb_color_int[1],rgb_color_int[2],rgb_color_int[3],max=255)
@@ -26,6 +17,7 @@ read_color = function(x){
 ############ read input files
 ####################################################
 ### read signal matrix file
+print('read signal matrix file')
 signal_matrix_od = as.matrix(read.table(index_matrix_ideas_state_inputfile, header=FALSE))
 ### extract signal matrix without info
 signal_matrix = signal_matrix_od[ , 2:dim(signal_matrix_od)[2] ]
@@ -37,21 +29,33 @@ colname = colname_file[,2]
 colnames(signal_matrix) = colname
 
 ### index set
+print('get uniq index set id')
 index_set_id = signal_matrix_od[,1]
 index_set_id_uniq = unique(index_set_id)
 ### sort index
 index_set_id_uniq_sort = sort(index_set_id_uniq)
 
 ### get unique elements from the matrix
+print('get uniq ideas label id')
 ideas_state_matrix_flatten = as.vector(signal_matrix)
 ideas_state_matrix_uniq = unique(ideas_state_matrix_flatten)
 ### sort index
 ideas_state_matrix_uniq_sort = sort(ideas_state_matrix_uniq)
 
+### set heatmap colors
+print('set heatmap colors')
+rgb_col_num=read.table(ideas_state_color,header=F)
+rgb_col_num=rgb_col_num[,3]
+print(rgb_col_num)
+rgb_col=apply(rgb_col_num,1,function(x) read_color(x))
+my_colorbar=colorRampPalette(rgb_col)(n = dim(rgb_col_num)[1])
+
 
 for (j in range(1, dim(index_set_id_uniq_sort)[1])){
+	print(paste('index set', toString(index_set_id_uniq_sort[j])))
 	signal_matrix_tmp = signal_matrix[index_set_id==index_set_id_uniq_sort[j],]
 	###### extract counts matrix
+	print('extract counts matrix')
 	counts_matrix = c()
 	for (i in c(1: dim(signal_matrix)[2]) ){
 		### extract ith cell type data
@@ -71,15 +75,8 @@ for (j in range(1, dim(index_set_id_uniq_sort)[1])){
 	### add colnames
 	colnames(counts_matrix_t) = colnames(signal_matrix)
 
-	### set heatmap colors
-	rgb_col_num=read.table(ideas_state_color,header=F)
-	rgb_col_num=rgb_col_num[,3]
-	print(rgb_col_num)
-	rgb_col=apply(rgb_col_num,1,function(x) read_color(x))
-
-	my_colorbar=colorRampPalette(rgb_col)(n = dim(rgb_col_num)[1])
-
 	### save figure
+	print('save figure')
 	pdf(paste(toString(index_set_id_uniq_sort[j]), '.', cREs_IDEASpro_outfile, sep=''), 16, 16)
 	barplot(counts_matrix_t, col=my_colorbar)
 	dev.off()
