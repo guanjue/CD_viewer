07875838c4ce40db941329ea769c200859ee0f3e
Guanjue Xiang <gzx103@E1-052111.science.psu.edu>
2018-02-19 15:20:29 -0500

    add path correlation

--- bin/plot_tree_cor.R
+++ bin/plot_tree_cor.R
@@ -78,8 +78,9 @@ for (i in seq(1,dim(signal_matrix)[1])){
 			match_id = match(path_names, colname)
 			print(match_id)
 			path_signal = signal_matrix[i,][match_id]
+			path_signal = path_signal[!is.na()]
 			print(path_signal)
-			path_sig_cor = cor(path_signal, range(1,length(path)), method = 'spearman')
+			path_sig_cor = cor(path_signal, range(1,length(path_signal)), method = 'spearman')
 			path_sig_cor_vector[i] = path_sig_cor
 		}
 	}
